a0b3b768c00e33da85519833e8971916e9919a3e
dior <diorbenskhodji@gmail.com>
2024-04-12 23:33:43 +0500

    mirrored

--- calculator/cmd/orchest/main.go
+++ calculator/cmd/orchest/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	pb "github.com/dusk-chancellor/go-gprc-examples/calculator/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+
+
+func main() {
+	conn, err := grpc.Dial("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("could not connect to grpc server: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	grpcClient := pb.NewExpressionServiceClient(conn)
+
+	ctx := context.TODO()
+	ans1, err := grpcClient.Calculate(ctx, &pb.ExpressionRequest{
+		Expression: "2+2*2",
+	})
+	if err != nil {
+		fmt.Println("failed: ", err)
+	}
+
+	fmt.Printf("2+2*2 = %v", ans1.Result)
 }
--- calculator/cmd/worker/main.go
+++ calculator/cmd/worker/main.go
@@ -1,5 +1,56 @@
 package main
 
 import (
-	pb "github.com/dusk-chancellor/go-grpc-examples/calculator/proto"
+	"context"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/dusk-chancellor/go-gprc-examples/calculator/internal/calculation"
+	"github.com/dusk-chancellor/go-gprc-examples/calculator/internal/infix_to_postfix"
+	pb "github.com/dusk-chancellor/go-gprc-examples/calculator/proto"
+	"google.golang.org/grpc"
 )
+
+type Server struct {
+	pb.ExpressionServiceServer
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+type ExpressionServiceServer interface {
+	Calculate(ctx context.Context, in *pb.ExpressionRequest) (*pb.ExpressionResponse, error)
+	mustEmbedUnimplementedGeometryServiceServer()
+}
+
+func (s *Server) Calculate(ctx context.Context, in *pb.ExpressionRequest) (*pb.ExpressionResponse, error) {
+	postfixed := infix_to_postfix.ToPostfix(in.Expression)
+	res, err := calculation.Evaluate(postfixed)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ExpressionResponse{Result: float32(res)}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:5000")
+	if err != nil {
+		fmt.Println("error starting tcp listener: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("tcp listener started")
+
+	grpcServer := grpc.NewServer()
+
+	exprServiceServer := NewServer()
+
+	pb.RegisterExpressionServiceServer(grpcServer, exprServiceServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		fmt.Println("error serving grpc: ", err)
+		os.Exit(1)
+	}
+}
